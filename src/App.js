8e8e1073093eb5c785de49052e28a00bc6daa029
RoyaAlmasimehr <almasimehrroya@gmail.com>
2023-12-09 11:41:05 +0330

    create dictionary

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+import Dictionary from "./Dictionary";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <footer className="App-footer">
-        This project was coded by Roya Almasimehr and is{" "}
-        <a href="https://github.com/RoyaAlmasimehr/dictionary-project">
-          open-sourced on Github
-        </a>{" "}
-        and{" "}
-        <a href="https://dictionary-project200.netlify.app/">
-          hosted on Netlify{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This project was coded by Roya Almasimehr and is{" "}
+          <a href="https://github.com/RoyaAlmasimehr/dictionary-project">
+            open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a href="https://dictionary-project200.netlify.app/">
+            hosted on Netlify{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
